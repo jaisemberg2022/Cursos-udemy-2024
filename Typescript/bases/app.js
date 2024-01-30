baf302c58dbc910397ecb5f4178d72e635c2f0df
Jaider Emanuel <Jaider Emanuel Toro Sanguino>
2024-01-30 15:31:21 -0500

    transpilacion de typescript a javascript

---
+++ Typescript/bases/app.js
@@ -0,0 +1,8 @@
+"use strict";
+const msg = "hola mundo !!!";
+const hero = {
+    name: 'ironman',
+    age: 45
+};
+hero.age = 50;
+console.log(hero.age + 1);
---
+++ Typescript/bases/app.ts
@@ -0,0 +1,9 @@
+const msg: string = "hola mundo !!!";
+
+const hero ={
+    name:'ironman',
+    age:45
+}
+
+hero.age = 50
+console.log(hero.age + 1)
