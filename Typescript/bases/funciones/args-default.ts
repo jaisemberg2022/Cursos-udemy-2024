73e7db3b39bcb506ab6b9e06d3c43b43e22c915a
Jaider Emanuel <Jaider Emanuel Toro Sanguino>
2024-01-31 14:07:40 -0500

    funciones,argumentos con valor definido por defecto y error entre secuencia de argumentos opcionales y obligatorios

---
+++ Typescript/bases/funciones/args-default.ts
@@ -0,0 +1,17 @@
+(()=>{
+
+    const fullName = (firstName:string,lastName?:string,upper:boolean = false):string =>{
+
+        if (upper) {
+        return `${firstName} , ${lastName || 'no last name'}`.toUpperCase();
+            
+        } else {
+        return `${firstName} , ${lastName || 'no last name'}`;   
+        }
+
+    }
+
+    const name = fullName('tony','stark');
+    console.log({name})
+
+})()
