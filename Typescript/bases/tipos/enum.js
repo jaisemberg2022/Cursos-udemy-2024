a029516fc59831a35f69a0ab71b9d67bffc5e845
Jaider Emanuel <Jaider Emanuel Toro Sanguino>
2024-01-31 11:38:32 -0500

    tipado enum,conjunto de valores fijos con corelacion de entre su definicon que se le puede asignar a una variable individual

---
+++ Typescript/bases/tipos/enum.js
@@ -0,0 +1,12 @@
+"use strict";
+(() => {
+    let aduioLevel;
+    (function (aduioLevel) {
+        aduioLevel[aduioLevel["min"] = 0] = "min";
+        aduioLevel[aduioLevel["medium"] = 1] = "medium";
+        aduioLevel[aduioLevel["max"] = 2] = "max";
+    })(aduioLevel || (aduioLevel = {}));
+    let currentAudio = aduioLevel.max;
+    console.log(currentAudio);
+    console.log(aduioLevel);
+})();
---
+++ Typescript/bases/tipos/enum.ts
@@ -0,0 +1,13 @@
+(()=>{
+
+    enum aduioLevel{
+        min,
+        medium,
+        max
+    }
+    let currentAudio = aduioLevel.max;
+
+    console.log(currentAudio);
+    console.log(aduioLevel);
+
+})()
