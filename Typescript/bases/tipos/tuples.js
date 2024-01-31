a31f87604ff30ab7908d0bdbc9799fc029809a1a
Jaider Emanuel <Jaider Emanuel Toro Sanguino>
2024-01-31 10:51:16 -0500

    tipado tupple,tipado personalizado y obligatorio a cada una de las posiciones de un arreglo

---
+++ Typescript/bases/tipos/tuples.js
@@ -0,0 +1,8 @@
+"use strict";
+(() => {
+    const hero = ['Dr strange', 100, false];
+    hero[0].toUpperCase();
+    hero[1] = 50;
+    hero[2] = true;
+    console.log({ hero });
+})();
---
+++ Typescript/bases/tipos/tuples.ts
@@ -0,0 +1,9 @@
+(() =>{
+
+    const hero:[string,number,boolean] = ['Dr strange',100,false];
+    hero[0].toUpperCase();
+    hero[1] = 50;
+    hero[2] = true;
+    console.log({hero})
+
+})()
