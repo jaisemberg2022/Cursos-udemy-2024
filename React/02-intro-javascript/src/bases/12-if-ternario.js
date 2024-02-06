61185a4c9be2bbe5decbdc15d82f85c7f0793683
Jaider Emanuel <Jaider Emanuel Toro Sanguino>
2024-02-06 09:44:48 -0500

    React fundamentos-js ,como simplificar una condicion if por medio de ternarios

---
+++ React/02-intro-javascript/src/bases/12-if-ternario.js
@@ -0,0 +1,14 @@
+const activo = true;
+// let mensaje = '';
+// if (activo) {
+//     mensaje ='activo'
+// }else{
+//     mensaje = 'inactivo'
+// }
+
+//if ternario corto
+const mensaje = (activo) ? 'Activo':'Inactivo';
+//if con un solo return
+const mensaje2 = activo && 'Activo';
+
+console.log(mensaje);
