ecacda953dc083c3a203550f26b35da7260eb299
Jaider Emanuel <Jaider Emanuel Toro Sanguino>
2024-02-07 09:37:04 -0500

    React vite pruebas unitarias ,metodo nativo toEqual y toStrictEqual de jest

---
+++ React/03-counter-app/src/tests/base-pruebas/05-funciones.test.js
@@ -0,0 +1,22 @@
+import { getUser, getUsuarioActivo } from '../../base-pruebas/05-funciones'
+
+describe("pruebas en 05-funciones", () => {
+  test("getUser debe retornar un objeto", () => {
+    const testUser = {
+      uid: "ABC123",
+      username: "El_Papi1502",
+    };
+    const user = getUser()
+    expect(testUser).toEqual(user);
+  });
+  test("getUsuarioActivo debe retornar un objeto",()=>{
+    const name ='Fernando';
+    const user = getUsuarioActivo(name);
+    expect(user).toStrictEqual({
+        uid:'ABC567',
+        username:name
+    })
+  });
+});
+
+
