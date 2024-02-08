ff0ea9aa0dc5b1bab4d3052e4c5f5515cfe81c1e
Jaider Emanuel <Jaider Emanuel Toro Sanguino>
2024-02-08 16:08:01 -0500

    React vite proyecto gif, como el uso del hook useEffect permite controlar el numero de veces que un comoponente es renderizado o en este ejemplo en numero total de peticiones http que se ejecutan cuandose ejecuta una funcion por medio de eventos

--- React/04-gif-expert-app/src/components/GifGrid.jsx
+++ React/04-gif-expert-app/src/components/GifGrid.jsx
@@ -1,28 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { ListItem, ListItemText, Divider } from "@mui/material";
-
-
-const getGifs = async (category) =>{
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=JiDu5FQO0WKmg87XDGxXqVrdk0P60ECZ&q=${category}&limit=20`;
-    const resp = await fetch(url);
-    const {data} = await resp.json();
-
-    const gifs = data.map(img => ({
-        id: img.id,
-        title: img.title,
-        url:img.images.downsized_medium.url
-    }))
-    console.log(gifs);
-    return gifs;
-}
-
-
-
-
-
+import { getGifs } from "../helpers/getGifs";
 
 export const GifGrid = ({ category }) => {
-getGifs(category);
+
+  useEffect(()=>{
+    getGifs(category);
+  },[]);
+    
 
   return (
     <>
