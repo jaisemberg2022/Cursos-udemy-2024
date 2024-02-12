af3a5c559e102d7e07540093a3b3c5f80c1af821
Jaider Emanuel <Jaider Emanuel Toro Sanguino>
2024-02-12 10:56:58 -0500

    React vite proyecto gif test, pruebas unitarias componente GifGrid, como los mocks de jest permiten especificar y controlar el tipo de informacion requerida para asi poder testear el ciclo de vida de informacion de un componente

--- React/04-gif-expert-app/src/test/components/GifGrid.test.jsx
+++ React/04-gif-expert-app/src/test/components/GifGrid.test.jsx
@@ -1,14 +1,43 @@
-import { render, screen, fireEvent, getByText, getByAltText } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { GifGrid } from "../../components";
+import { expect, jest, test } from "@jest/globals";
+
+import { useFetchGifs } from "../../hooks/useGetchsGif";
+
+jest.mock("../../hooks/useGetchsGif");
 
 describe("pruebas en componente <GifGrid/>", () => {
   const category = "Silent hill";
   test("Debe mostrar el loding inicialmente", () => {
+    useFetchGifs.mockReturnValue({
+      images: [],
+      isLoading: true,
+    });
     render(<GifGrid category={category} />);
-    screen.debug();
     expect(screen.queryByText("Cargando..."));
     expect(screen.getByText(category));
   });
 
-
+  test("Debe de mostrat items cuando se cargan las imagenes useFetchGuf", () => {
+    
+    const gifs = [
+      {
+        id:'ABC',
+        tittle:'Algo...',
+        url:'https://loscalhost/Algo.jpg'
+      },
+      {
+        id:'EFG',
+        tittle:'Algo2...',
+        url:'https://loscalhost/Algo2.jpg'
+      }
+    ]
+    
+    useFetchGifs.mockReturnValue({
+      images:gifs,
+      isLoading:true
+    })
+    render(<GifGrid category={category} />);
+    expect(screen.getAllByRole('img').length).toBe(2)
+  });
 });
