3287643df72aec670326cf67b19fe29d98ce301c
Jaider Emanuel <Jaider Emanuel Toro Sanguino>
2024-02-12 08:28:31 -0500

    React vite proyecto gif test, pruebas unitarias componente AddCategory

--- React/04-gif-expert-app/src/components/AddCategory.jsx
+++ React/04-gif-expert-app/src/components/AddCategory.jsx
@@ -1,35 +1,36 @@
 import TextField from "@mui/material/TextField";
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
 import { useState } from "react";
 
-export const AddCategory = ({onNewCategory}) => {
+export const AddCategory = ({ onNewCategory }) => {
   const [inputValue, setInputValue] = useState('');
-  const onInputChange = ({target})=>{
-    setInputValue(target.value)
-  }
-  const onSubmit = (event)=>{
+
+  const onInputChange = ({ target }) => {
+    setInputValue(target.value);
+  };
+  const onSubmit = (event) => {
     event.preventDefault();
-    if (inputValue.trim().lenght <=1 ) {return}
+    if ( inputValue.trim().length <= 1 ) return; 
     // setCategories( (categories) =>[inputValue,...categories]);
-    onNewCategory(inputValue.trim())
-    setInputValue('');
-  }
+    setInputValue("");
+    onNewCategory(inputValue.trim());
+  };
   return (
     <form onSubmit={onSubmit} aria-label="form">
-        <TextField
-          value =  { inputValue || ''}
-          label="filtrar"
-          variant="outlined"
-          placeholder="Buscar gifs..."
-          InputLabelProps={{ shrink: true }}
-          onChange={onInputChange}
-          autoComplete="off"
-          size="small"
-        />
+      <TextField
+        value={inputValue || ""}
+        label="filtrar"
+        variant="outlined"
+        placeholder="Buscar gifs..."
+        InputLabelProps={{ shrink: true }}
+        onChange={onInputChange}
+        autoComplete="off"
+        size="small"
+      />
     </form>
   );
 };
 
 AddCategory.propTypes = {
-  onNewCategory:PropTypes.func.isRequired
-}
+  onNewCategory: PropTypes.func.isRequired,
+};
