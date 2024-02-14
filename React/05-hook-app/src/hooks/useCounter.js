10ef45376c5258ac527c334c82f990501a8d48f4
Jaider Emanuel <Jaider Emanuel Toro Sanguino>
2024-02-14 11:13:43 -0500

    React vite proyecto useContext,como la funcion act garantiza que todas las actualizaciones relacionadas con los componentes de React (como cambios de estado, efectos, etc.) se procesen y apliquen antes de pasar a la siguiente operación de su prueba,ademas de configurar las funciones del mismo hook para asegurarse de siempre obtener elvalor actual del contador al momento de editar su estado

--- React/05-hook-app/src/hooks/useCounter.js
+++ React/05-hook-app/src/hooks/useCounter.js
@@ -1,10 +1,10 @@
 import { useState } from "react"
 export const useCounter = (initialValue = 10) =>{
     const [counter, setCounter] = useState(initialValue);
-    const increment = (valueIncrement = 1) => setCounter(counter + valueIncrement);
+    const increment = (valueIncrement = 1) => setCounter((counter) => counter + valueIncrement);
     const decrement = (valueDecrement = 1) => {
         if (counter === 0 || valueDecrement <=0) return;
-        setCounter(counter - valueDecrement);
+        setCounter((counter) =>counter - valueDecrement);
     };
     const restore = () => setCounter(initialValue);
     return{
--- React/05-hook-app/test/hooks/useCounter.test.js
+++ React/05-hook-app/test/hooks/useCounter.test.js
@@ -1,4 +1,4 @@
-import {renderHook} from '@testing-library/react'
+import {renderHook,act} from '@testing-library/react'
 import { useCounter } from '../../src/hooks/useCounter'
 
 
@@ -16,6 +16,42 @@ describe('first', () => {
         const {result}= renderHook(()=> useCounter(100));
         const {counter} = result.current;
         expect(counter).toBe(100);
-    })
+    });
+
+    test('Debe de incrementar el contador',()=>{
+        const {result}= renderHook(()=> useCounter(100));
+        const {counter,decrement,increment,restore} = result.current;  
+
+        act(()=>{
+            increment();
+            increment(2);
+        });
+
+        expect(result.current.counter).toBe(103)
+    });
+
+    test('Debe de decrementar el contador',()=>{
+        const {result}= renderHook(()=> useCounter(100));
+        const {counter,decrement,increment,restore} = result.current;  
+
+        act(()=>{
+            decrement();
+            decrement(2);
+        });
+
+        expect(result.current.counter).toBe(97)
+    });
+
+    test('Debe de restaurar el contador',()=>{
+        const {result}= renderHook(()=> useCounter(100));
+        const {counter,decrement,increment,restore} = result.current;  
+
+        act(()=>{
+            restore()
+        });
+
+        expect(result.current.counter).toBe(100)
+    });
+
 
  })
