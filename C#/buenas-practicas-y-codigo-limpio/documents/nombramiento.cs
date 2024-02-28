c2f04744092b99b0f50799f51b1ed62095b415e0
Jaider Emanuel <Jaider Emanuel Toro Sanguino>
2024-02-28 17:04:48 -0500

    C# buenas paracticas y codigo limpio, nombramiento variables,metodos y clases

---
+++ C#/buenas-practicas-y-codigo-limpio/documents/nombramiento.cs
@@ -0,0 +1,31 @@
+/*NOMBREMIETNO*/
+
+/* VARIABLES:
+    1).el nombre de las variables debe ser lo mas descriptivo posible de acuerdo a lo que va a almacear
+*/
+
+    // mal
+    var string nbr
+    // bien
+    var string nombre
+
+/*METODOS: 
+    1).el nombre de las variables debe ser lo mas descriptivo posible de acuerdo a la accion que va a realizar 
+    2).utilizar el sitema de nombramiento 'pascalCase'
+*/
+
+    // mal
+    public List<Users>getUsers();
+    // bien 
+    public List <Users> GetActiveUsers();
+
+/*CLASES:
+    1).el nombre de las clases debe ser lo mas descriptivo posible.
+    2).no llevar la plabra CLASS en su nombre (Notación húngara).
+    3).utilizar directamente numeros en sus nombres 
+*/    
+
+    // mal
+    public class ClassUser2
+    // bien
+    public class User
