c8fb922f6cc53ba9093f62e3d407ac293b826763
Jaider Emanuel <Jaider Emanuel Toro Sanguino>
2024-03-12 15:56:42 -0500

    C# fundamentos de .NET,paquetes nuget y como instalarlos

--- C#/fundamenteos-de-NET/projects/proyecto-de-consola/Program.cs
+++ C#/fundamenteos-de-NET/projects/proyecto-de-consola/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Humanizer;
+
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Porfavor ingrese un nombre");
+var nombre = Console.ReadLine();
+Console.WriteLine("Porfavor ingrese su cargo");
+var cargo = Console.ReadLine();
+Console.WriteLine("Porfavor ingrese su edad");
+var edad = int.Pase(Console.ReadLine());
+
+
+Console.WriteLine($"hola,mi nombre es {nombre}, mi cargo es {cargo}, y tengo {edad.ToWords(new System.Globalization.CultureInfo("es"))} años");
+
+
+
