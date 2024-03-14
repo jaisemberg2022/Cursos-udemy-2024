13060c08b37d0255a001df42618f75051560268c
Jaider Emanuel <Jaider Emanuel Toro Sanguino>
2024-03-14 11:24:34 -0500

    C# fundamentos entity framework, estructuracion y creacion de tablas con relacion por medio de fluent-api para recrear la base de datos

--- C#/fundamentos-de-entity-framework/aplicacion-web/tareasContext.cs
+++ C#/fundamentos-de-entity-framework/aplicacion-web/tareasContext.cs
@@ -2,14 +2,35 @@ namespace proyectoef;
 using Microsoft.EntityFrameworkCore;
 using proyectoef.Models;
 
-class TareasContext:DbContext
+class TareasContext : DbContext
 {
     public DbSet<Categoria> categorias { get; set; }
-    public DbSet<Tarea> tareas {get;set;}
+    public DbSet<Tarea> tareas { get; set; }
 
-    public TareasContext(DbContextOptions<TareasContext> options):base( options)
+    public TareasContext(DbContextOptions<TareasContext> options) : base(options) { }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        
+        modelBuilder.Entity<Categoria>(categoria =>
+        {
+            categoria.ToTable("Categoria");
+            categoria.HasKey(p => p.categoriaId);
+            categoria.Property(p => p.nombre).IsRequired().HasMaxLength(150);
+            categoria.Property(p => p.descripcion);
+        });
+
+        modelBuilder.Entity<Tarea>(tarea =>
+        {
+            tarea.ToTable("Tarea");
+            tarea.HasKey(t => t.TareaId);
+            tarea.HasOne(t => t.Categoria).WithMany(p => p.Tareas).HasForeignKey(p => p.CategoriaId);
+            tarea.Property(t=>t.titulo).IsRequired().HasMaxLength(200);
+            tarea.Property(t=>t.descripcion);
+            tarea.Property(t=>t.PrioridadTarea);
+            tarea.Property(t=>t.FechaCreacion);
+            tarea.Ignore(p=> p.Resumen);
+
+
+        });
     }
-    
+
 }
